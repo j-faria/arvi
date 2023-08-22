9b04452653b0895865bb39f320377024bee04021
João Faria <joao.faria@astro.up.pt>
2023-08-22 17:54:30 +0200

    dev

--- arvi/__init__.py
+++ arvi/__init__.py
@@ -4,27 +4,10 @@ __all__ = []
 from .timeseries import RV
 
 _ran_once = False
+
 def __getattr__(name: str):
-    global _ran_once
+    global _ran_once  # can't do it any other way :(
     if _ran_once:
         return RV(name)
     else:
         _ran_once = True
-
-
-# from importlib import import_module
-
-# HACK_IMPORTS = True
-
-# if HACK_IMPORTS:
-#     timeseries = import_module('.timeseries', 'arvi')
-#     class FakeModule(object):
-#         def __getattribute__(self, name):
-#             print(name)
-#             r = RV(name)
-#             return r
-#     # first, load (some) subpackages
-#     # then, replace the 'arvi' module to allow for dynamic imports
-#     # of the type "from arvi import HD1"
-#     import sys
-#     sys.modules['arvi'] = FakeModule()
