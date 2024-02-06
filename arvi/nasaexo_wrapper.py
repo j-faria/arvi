a45804d41d2da2d02a3c9dfdacd793b486d6da8d
João Faria <joao.faria@unige.ch>
2024-02-06 17:14:47 +0100

    fix issues reading from nasa exo database

--- arvi/nasaexo_wrapper.py
+++ arvi/nasaexo_wrapper.py
@@ -2,7 +2,9 @@ from copy import copy
 
 import requests
 from io import StringIO
+
 import numpy as np
+from astropy.timeseries import LombScargle
 
 from .setup_logger import logger
 from kepmodel.rv import RvModel
@@ -46,51 +48,97 @@ class Planets:
 
         # try again with other ids
         if self.np == 0:
-            hdname = [i for i in self.s.simbad.ids if 'HD' in i]
-            if len(hdname) != 0:
-                hdname = hdname[0]
-                if self.verbose:
-                    logger.info(f"trying with the HD name '{hdname}'...")
-                STAR_QUERY_HD = STAR_QUERY[:-1]
-                STAR_QUERY_HD.append(f"hd_name like '{hdname}'")
-                query = ' '.join(STAR_QUERY_HD).replace(' ', '+')
-                self.response, self.data = run_query(query)
-                self.np = self.data.size
-            
-            hipname = [i for i in self.s.simbad.ids if 'HIP' in i]
-            if len(hipname) != 0:
-                hipname = hipname[0]
-                if self.verbose:
-                    logger.info(f"trying with the HIP name '{hipname}'...")
-                STAR_QUERY_HIP = STAR_QUERY[:-1]
-                STAR_QUERY_HIP.append(f"hip_name like '{hipname}'")
-                query = ' '.join(STAR_QUERY_HIP).replace(' ', '+')
-                self.response, self.data = run_query(query)
-                self.np = self.data.size
+            try:
+                if hasattr(self.s, 'hd'):
+                    hdname = [self.s.hd]
+                else:
+                    hdname = [i for i in self.s.simbad.ids if 'HD' in i]
+                if len(hdname) != 0:
+                    hdname = hdname[0]
+                    if self.verbose:
+                        logger.info(f"trying with the HD name '{hdname}'...")
+                    STAR_QUERY_HD = STAR_QUERY[:-1]
+                    STAR_QUERY_HD.append(f"hd_name like '{hdname}'")
+                    query = ' '.join(STAR_QUERY_HD).replace(' ', '+')
+                    self.response, self.data = run_query(query)
+                    self.np = self.data.size
+                
+                hipname = [i for i in self.s.simbad.ids if 'HIP' in i]
+                if len(hipname) != 0:
+                    hipname = hipname[0]
+                    if self.verbose:
+                        logger.info(f"trying with the HIP name '{hipname}'...")
+                    STAR_QUERY_HIP = STAR_QUERY[:-1]
+                    STAR_QUERY_HIP.append(f"hip_name like '{hipname}'")
+                    query = ' '.join(STAR_QUERY_HIP).replace(' ', '+')
+                    self.response, self.data = run_query(query)
+                    self.np = self.data.size
+            except AttributeError:
+                pass
 
         if self.verbose:
             logger.info(f'found {self.np} planets')
 
+        self.create_model()
+
+    def create_model(self, instrument=None, strict=False,
+                     all_instruments_except=None, **kwargs):
+        if instrument is None and all_instruments_except is None:
+            ts = self.s._mtime_sorter
+            self.model = RvModel(self.s.mtime[ts], self.s.mvrad[ts],
+                                err=Error(self.s.msvrad[ts]))
+            for inst in self.s.instruments:
+                if inst not in self.s.instrument_array[self.s.mask]:
+                    continue
+                self.model.add_lin(derivative=1.0*(self.s.instrument_array[self.s.mask][ts]==inst),
+                                   name=f'offset_inst_{inst}',
+                                   value=getattr(self.s, inst).mvrad.mean())
+        else:
+            if all_instruments_except is not None:
+                exception = self.s._check_instrument(all_instruments_except, strict)
+                instruments = copy(self.s.instruments)
+                for e in exception:
+                    instruments.pop(instruments.index(e))
+            else:
+                instruments = self.s._check_instrument(instrument, strict)
+            mask = np.in1d(self.s.instrument_array, instruments)
+            ts = self.s._time_sorter[mask]
+            self.model = RvModel(self.s.time[ts], self.s.vrad[ts],
+                                err=Error(self.s.svrad[ts]))
+            for inst in instruments:
+                self.model.add_lin(1.0*(self.s.instrument_array[ts]==inst),
+                                   f'offset_inst_{inst}')
         self.set_parameters()
 
     def set_parameters(self):
         self.P = np.atleast_1d(self.data['pl_orbper'])
         self.K = np.atleast_1d(self.data['pl_rvamp'])
         self.e = np.atleast_1d(self.data['pl_orbeccen'])
+        self.w = np.atleast_1d(self.data['pl_orblper'])
 
-        ts = self.s._time_sorter
-        self.model = RvModel(self.s.time[ts], self.s.vrad[ts],
-                             err=Error(self.s.svrad[ts]))
+        if self.P.size == 0:
+            return
 
-        for inst in self.s.instruments:
-            self.model.add_lin(1.0*(self.s.instrument_array[ts]==inst), f'offset_inst_{inst}')
+        if self.model.nkep > 0:
+            for i in range(self.np):
+                self.model.rm_keplerian(f'{i}')
+            self.model.keplerian.uid = 0
 
         for i in range(self.np):
-            if self.K[i] == False:
+            if self.K[i] == False or np.isnan(self.K[i]):
                 self.model.add_keplerian_from_period(self.P[i], fit=True)
             else:
-                self.model.add_keplerian([self.P[i], self.K[i], self.e[i], 0.0, 0.0],
+                self.model.add_keplerian([self.P[i], self.K[i], self.e[i], 0.0, self.w[i]],
                                         ['P', 'K', 'e', 'M0', 'omega'], fit=True)
+
+    def add_planet_from_period(self, period):
+        self.model.add_keplerian_from_period(period, fit=True)
+        self.np += 1
+    
+    def add_keplerian_from_periodogram(self):
+        gls = LombScargle(self.model.t, self.model.y - self.model.model())
+        freq, power = gls.autopower()
+        self.add_planet_from_period(1/freq[power.argmax()])
 
     def fit_lin(self, adjust_data=True):
         self.model.show_param()
@@ -99,7 +147,10 @@ class Planets:
         if adjust_data:
             for inst in self.s.instruments:
                 _s = getattr(self.s, inst)
-                _s.vrad -= self.model.get_param(f'lin.offset_inst_{inst}')
+                try:
+                    _s.vrad -= self.model.get_param(f'lin.offset_inst_{inst}')
+                except ValueError:
+                    pass
             self.s._build_arrays()
 
     def fit_angles(self):
@@ -129,7 +180,10 @@ class Planets:
         if adjust_data:
             for inst in self.s.instruments:
                 _s = getattr(self.s, inst)
-                _s.vrad -= self.model.get_param(f'lin.offset_inst_{inst}')
+                try:
+                    _s.vrad -= self.model.get_param(f'lin.offset_inst_{inst}')
+                except ValueError:
+                    pass
             self.s._build_arrays()
 
     def __repr__(self):
