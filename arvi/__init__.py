0523fc4dadd5469b573ccadd3604dd2163b73fd7
João Faria <joao.faria@unige.ch>
2024-03-18 18:37:15 +0100

    issue with import arvi

--- arvi/__init__.py
+++ arvi/__init__.py
@@ -5,6 +5,12 @@ from .timeseries import RV
 _ran_once = False
 
 def __getattr__(name: str):
+    if name in (
+        '_ipython_canary_method_should_not_exist_',
+        '_repr_mimebundle_',
+    ):
+        return
+
     global _ran_once  # can't do it any other way :(
     if _ran_once:
         return RV(name)
