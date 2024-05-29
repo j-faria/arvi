28cc3ba85223c4eba09c789975e67139f9c7567a
João Faria <joao.faria@unige.ch>
2024-05-29 14:49:57 +0200

    add translations for NGC open cluster targets

--- arvi/translations.py
+++ arvi/translations.py
@@ -1,3 +1,5 @@
+import re
+
 STARS = {
     'Barnard': 'GJ699',
     "Barnard's": 'GJ699',
@@ -5,6 +7,15 @@ STARS = {
 
 
 def translate(star):
+    # known translations
     if star in STARS:
         return STARS[star]
+
+    # regex translations
+    NGC_match = re.match(r'NGC([\s\d]+)No([\s\d]+)', star)
+    if NGC_match:
+        cluster = NGC_match.group(1).replace(' ', '')
+        target = NGC_match.group(2).replace(' ', '')
+        return f'Cl* NGC {cluster} MMU {target}'
+
     return star
