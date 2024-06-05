13d7f7682f9c8ba29ec45424409298148b8b6e50
João Faria <joao.faria@unige.ch>
2024-06-05 16:46:52 +0200

    add module for dealing with spectra

---
+++ arvi/spectra.py
@@ -0,0 +1,117 @@
+import os
+from glob import glob
+import numpy as np
+import matplotlib.pyplot as plt
+
+from .setup_logger import logger
+
+import astropy.units as u, astropy.constants as const
+from astropy.io import fits
+
+def doppler_shift(wave: np.ndarray, flux: np.ndarray, velocity: float):
+    """ Doppler shift a spectrum by a given velocity
+
+    Args:
+        wave (np.ndarray): wavelength array
+        flux (np.ndarray): flux array
+        velocity (float): velocity in km/s
+    """
+    c = const.c.to(u.km/u.second).value
+    doppler_factor = np.sqrt((1 + velocity/c) / (1 - velocity/c))
+    new_wavelength = wave * doppler_factor
+    new_flux = np.interp(new_wavelength, wave, flux)
+    return new_wavelength, new_flux
+
+def fit_gaussian_to_line(wave, flux, center_wavelength, around=0.15 * u.angstrom,
+                         plot=True):
+    from scipy.optimize import curve_fit
+    if center_wavelength < wave.min() or center_wavelength > wave.max():
+        raise ValueError('`center_wavelength` is outside the wavelength range')
+    if center_wavelength < wave[np.nonzero(flux)].min() or center_wavelength > wave[np.nonzero(flux)].max():
+        raise ValueError('`center_wavelength` is outside the wavelength range where flux is not zero')
+
+    try:
+        wave <<= u.angstrom
+    except u.UnitConversionError as e:
+        raise ValueError(f'could not convert `wave` to Angstroms: {e}') from None
+
+    try:
+        center_wavelength <<= u.angstrom
+    except u.UnitConversionError as e:
+        raise ValueError(f'could not convert `center_wavelength` to Angstroms: {e}') from None
+
+    try:
+        around <<= u.angstrom
+    except u.UnitConversionError as e:
+        raise ValueError(f'could not convert `around` to Angstroms: {e}') from None
+
+
+    def gaussian(x, amp, cen, wid, off):
+        return amp * np.exp(-(x-cen)**2 / (2*wid**2)) + off
+
+    wave_around = (wave > center_wavelength - around) & (wave < center_wavelength + around)
+    w, f = wave[wave_around].value, flux[wave_around]
+    lower, upper = np.array([
+        [-np.inf, 0], 
+        [-np.inf, np.inf], 
+        [0.0, 0.11], 
+        [0.9*f.max(), 1.1*f.max()]
+    ]).T
+    popt, pcov = curve_fit(gaussian, w, f, p0=[-f.ptp(), center_wavelength.value, 0.1, f.max()], bounds=(lower, upper))
+    EW = A = (np.sqrt(2) * np.abs(popt[0]) * np.abs(popt[2]) * np.sqrt(np.pi)) / popt[3]
+    perr = np.sqrt(np.diag(pcov))
+    EW_err = (np.sqrt(2) * np.abs(perr[0]) * np.abs(perr[2]) * np.sqrt(np.pi)) / perr[3]
+
+    if plot:
+        plt.plot(wave[wave_around], flux[wave_around], 'ko', ms=4, zorder=1)
+        plt.plot(wave[wave_around], flux[wave_around] - gaussian(w, *popt), 'o', ms=2)
+        wave_around_plot = (wave > center_wavelength - 2*around) & (wave < center_wavelength + 2*around)
+        # plt.plot(wave[wave_around_plot], flux[wave_around_plot], 'o', ms=2)
+        w = wave[wave_around_plot].value
+        plt.plot(w, gaussian(w, *popt), 'r-')
+        plt.fill_between([popt[1]-A, popt[1]+A], popt[3]+popt[0], popt[3],
+                         color='C2', alpha=0.1, lw=0)
+    return popt, EW*1e3, EW_err*1e3
+
+def detrend(w, f):
+    if w.shape[0] > w.shape[1]:
+        w = np.copy(w).T
+        f = np.copy(f).T
+
+    f_detrended = np.zeros_like(f)
+    for i, (ww, ff) in enumerate(zip(w, f)):
+        m = np.nonzero(ff)
+        fit = np.polyval(np.polyfit(ww[m] - np.median(ww[m]), ff[m], 1), ww - np.median(ww[m]))
+        f_detrended[i] = ff - fit + np.median(ff[m])
+    return w, f_detrended
+
+def build_master(self, limit=None, plot=True):
+    files = sorted(glob(f'{self.star}_downloads/*S1D_A.fits'))
+    if self.verbose:
+        logger.info(f'Found {len(files)} S1D files')
+
+    files = files[:limit]
+
+    if plot:
+        fig, axs = plt.subplots(2, 1, figsize=(9, 6), sharex=True, constrained_layout=True)
+        for ax in axs:
+            ax.set(xlabel=r'wavelength air [$\AA$]', ylabel='flux')
+        axs[0].set_title(self.star, loc='right', fontsize=10)
+
+    w0 = fits.getdata(files[0])['wavelength_air']
+    master_flux = np.zeros_like(w0)
+    for file in files:
+        rv = fits.getval(file, '*CCF RV')[0]
+        flux = fits.getdata(file)['flux']
+        _, new_flux = doppler_shift(w0, flux, rv)
+        master_flux += new_flux
+        if plot:
+            axs[0].plot(w0, new_flux, alpha=0.5)
+
+    master_flux /= len(files)
+    if plot:
+        axs[1].plot(w0, master_flux, 'k', label='master')
+        axs[1].legend()
+        axs[0].legend([], [], title=f'{len(files)} S1D spectra')
+
+    return w0, master_flux
