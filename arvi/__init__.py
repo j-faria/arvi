420f307ed670fd09572ebb4ca56fbc97933375e5
João Faria <joao.faria@unige.ch>
2024-06-24 01:15:34 +0200

    new way to import from arvi

--- arvi/__init__.py
+++ arvi/__init__.py
@@ -2,18 +2,32 @@ __all__ = ['RV']
 
 from .timeseries import RV
 
-_ran_once = False
+## OLD
+# # the __getattr__ function is always called twice, so we need this
+# # to only build and return the RV object on the second time
+# _ran_once = False
 
 def __getattr__(name: str):
     if name in (
         '_ipython_canary_method_should_not_exist_',
+        '_ipython_display_',
         '_repr_mimebundle_',
         '__wrapped__'
     ):
         return
 
-    global _ran_once  # can't do it any other way :(
-    if _ran_once:
-        return RV(name)
-    else:
-        _ran_once = True
+    try:
+        globals()[name] = RV(name)
+        return globals()[name]
+    except ValueError:
+        raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+
+    ## OLD
+    # # can't do it any other way :(
+    # global _ran_once
+
+    # if _ran_once:
+    #     _ran_once = False
+    #     return RV(name)
+    # else:
+    #     _ran_once = True
