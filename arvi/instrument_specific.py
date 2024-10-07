7798d49333cb70a71f25541b0325e94998811952
João Faria <joao.faria@unige.ch>
2024-10-07 20:12:38 +0200

    known_issues can optionally not mask the points

--- arvi/instrument_specific.py
+++ arvi/instrument_specific.py
@@ -90,10 +90,12 @@ def divide_HARPS(self):
 # ESPRESSO ADC issues
 from .utils import ESPRESSO_ADC_issues
 
-def ADC_issues(self, plot=True, check_headers=False):
-    """ Identify and mask points affected by ADC issues (ESPRESSO).
+def ADC_issues(self, mask=True, plot=True, check_headers=False):
+    """ Identify and optionally mask points affected by ADC issues (ESPRESSO).
 
     Args:
+        mask (bool, optional):
+            Whether to mask out the points.
         plot (bool, optional):
             Whether to plot the masked points.
         check_headers (bool, optional):
@@ -126,21 +128,23 @@ def ADC_issues(self, plot=True, check_headers=False):
         logger.info(f"there {'are'[:n^1]}{'is'[n^1:]} {n} frame{'s'[:n^1]} "
                      "affected by ADC issues")
 
-    self.mask[intersect] = False
-    self._propagate_mask_changes()
+    if mask:
+        self.mask[intersect] = False
+        self._propagate_mask_changes()
 
-    if plot:
-        self.plot(show_masked=True)
+        if plot:
+            self.plot(show_masked=True)
 
     return intersect
 
 # ESPRESSO cryostat issues
 from .utils import ESPRESSO_cryostat_issues
 
-def blue_cryostat_issues(self, plot=True):
+def blue_cryostat_issues(self, mask=True, plot=True):
     """ Identify and mask points affected by blue cryostat issues (ESPRESSO).
 
     Args:
+        mask (bool, optional): Whether to mask out the points.
         plot (bool, optional): Whether to plot the masked points.
     """
     instruments = self._check_instrument('ESPRESSO')
@@ -161,11 +165,12 @@ def blue_cryostat_issues(self, plot=True):
         logger.info(f"there {'are'[:n^1]}{'is'[n^1:]} {n} frame{'s'[:n^1]} "
                      "affected by blue cryostat issues")
 
-    self.mask[intersect] = False
-    self._propagate_mask_changes()
+    if mask:
+        self.mask[intersect] = False
+        self._propagate_mask_changes()
 
-    if plot:
-        self.plot(show_masked=True)
+        if plot:
+            self.plot(show_masked=True)
 
     return intersect
 
@@ -216,20 +221,21 @@ def qc_scired_issues(self, plot=False, **kwargs):
     return affected
 
 
-def known_issues(self, plot=False, **kwargs):
-    """ Identify and mask known instrumental issues (ADC and blue cryostat for ESPRESSO)
+def known_issues(self, mask=True, plot=False, **kwargs):
+    """ Identify and optionally mask known instrumental issues (ADC and blue cryostat for ESPRESSO)
 
     Args:
+        mask (bool, optional): Whether to mask out the points.
         plot (bool, optional): Whether to plot the masked points.
     """
     try:
-        adc = ADC_issues(self, plot, **kwargs)
+        adc = ADC_issues(self, mask, plot, **kwargs)
     except IndexError as e:
         # logger.error(e)
         logger.error('are the data binned? cannot proceed to mask these points...')
 
     try:
-        cryostat = blue_cryostat_issues(self, plot)
+        cryostat = blue_cryostat_issues(self, mask, plot)
     except IndexError as e:
         # logger.error(e)
         logger.error('are the data binned? cannot proceed to mask these points...')
