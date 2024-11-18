b3d71b95745a9c6296fc2587f353f32f14164c57
João Faria <joao.faria@unige.ch>
2024-11-18 23:29:20 +0100

    fix bug when npipe=1

--- arvi/dace_wrapper.py
+++ arvi/dace_wrapper.py
@@ -62,7 +62,7 @@ def get_arrays(result, latest_pipeline=True, ESPRESSO_mode='HR11', NIRPS_mode='H
             if 'NIRPS' in inst and any(['LBL' in p for p in pipelines]):
                 # TODO: correctly load both CCF and LBL
                 pipelines = [pipelines[1]]
-            if 'HARPS' in inst and pipelines[1] == pipelines[0] + '-EGGS':
+            if 'HARPS' in inst and npipe > 1 and pipelines[1] == pipelines[0] + '-EGGS':
                 pipelines = pipelines[:2]
             else:
                 pipelines = [pipelines[0]]
