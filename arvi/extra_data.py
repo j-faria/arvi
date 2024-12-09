af947d42a74bb2c71ca4c390b3f985bee30b574b
João Faria <joao.faria@unige.ch>
2024-12-09 12:15:04 +0100

    improve loading extra data

--- arvi/extra_data.py
+++ arvi/extra_data.py
@@ -10,7 +10,8 @@ refs = {
     'HD86226': 'Teske et al. 2020 (AJ, 160, 2)'
 }
 
-def get_extra_data(star, instrument=None, path=None, verbose=True):
+def get_extra_data(star, instrument=None, path=None, verbose=True,
+                   check_for_kms=True):
     if path is None:
         path = os.path.dirname(__file__)
         path = os.path.join(path, 'data', 'extra')
@@ -18,7 +19,7 @@ def get_extra_data(star, instrument=None, path=None, verbose=True):
     metadata = json.load(open(os.path.join(path, 'metadata.json'), 'r'))
     # print(metadata)
 
-    files = glob(os.path.join(path, star + '*'))
+    files = glob(os.path.join(path, star.replace(' ', '') + '*.rdb'))
     files = [f for f in files if os.path.isfile(f)]
     files = [f for f in files if not os.path.basename(f).endswith('.zip')]
 
@@ -57,9 +58,24 @@ def get_extra_data(star, instrument=None, path=None, verbose=True):
             if 'corrected_for_secular_acceleration' in metadata[file_basename]:
                 did_sa[i] = metadata[file_basename]['corrected_for_secular_acceleration']
 
-    s = timeseries.RV.from_rdb(files[0], star=star, instrument=instruments[0], units=units[0])
-    for file, instrument, unit in zip(files[1:], instruments[1:], units[1:]):
-        s = s + timeseries.RV.from_rdb(file, star=star, instrument=instrument, units=unit)
+    with logger.contextualize(indent='  '):
+        s = timeseries.RV.from_rdb(files[0], star=star, instrument=instruments[0], units=units[0])
+        if check_for_kms and s.svrad.min() < 0.01:
+            units[0] = 'kms'
+            s = timeseries.RV.from_rdb(files[0], star=star, instrument=instruments[0], units=units[0])
+        if verbose:
+            logger.info(f'{instruments[0]:>12s} ├ ({s.N} observations)')
+
+        for file, instrument, unit in zip(files[1:], instruments[1:], units[1:]):
+            _s = timeseries.RV.from_rdb(file, star=star, instrument=instrument, units=unit)
+            if check_for_kms and _s.svrad.min() < 0.01:
+                unit = 'kms'
+                _s = timeseries.RV.from_rdb(file, star=star, instrument=instrument, units=unit)
+            if verbose:
+                logger.info(f'{instrument:>12s} ├ ({_s.N} observations)')
+
+            s = s + _s
+
 
     for i, (inst, ref, inst_did_sa) in enumerate(zip(s.instruments, reference, did_sa)):
         _s = getattr(s, inst)
