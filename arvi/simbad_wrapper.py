d3f98486f219114c3c40e2d3d26ea6cc0bdfa0e1
João Faria <joao.faria@unige.ch>
2025-02-06 16:48:00 +0100

    improve results from Simbad query

--- arvi/simbad_wrapper.py
+++ arvi/simbad_wrapper.py
@@ -1,7 +1,14 @@
 import os
+import numpy as np
 import requests
+from dataclasses import dataclass
 
 import pysweetcat
+
+try:
+    from uncertainties import ufloat
+except ImportError:
+    ufloat = lambda x, y: x
 
 from .translations import translate
 
@@ -15,15 +22,31 @@ SELECT basic.OID,
        main_id,
        pmra,
        pmdec,
-       plx_value,
+       plx_value, plx_err,
        rvz_radvel,
        sp_type
 FROM basic JOIN ident ON oidref = oid
 WHERE id = '{star}';
 """
 
+# SELECT filter, flux, flux_err
+# FROM basic JOIN ident ON oid = ident.oidref JOIN flux ON oid = flux.oidref
+# WHERE id = 'HD23079';
+
 BV_QUERY = """
 SELECT B, V FROM allfluxes
+JOIN ident USING(oidref)
+WHERE id = '{star}';
+"""
+
+FILTERS_QUERY = """
+SELECT filter, flux, flux_err, bibcode FROM flux 
+JOIN ident USING(oidref)
+WHERE id = '{star}';
+"""
+
+MEAS_QUERY = """
+SELECT teff, log_g, log_g_prec, fe_h, fe_h_prec, bibcode FROM mesFe_H 
 JOIN ident USING(oidref)
 WHERE id = '{star}';
 """
@@ -40,6 +63,13 @@ JOIN ident ON oidref = oid
 WHERE id = '{star}';
 """
 
+@dataclass
+class Measurements:
+    teff: list
+    logg: list
+    feh: list
+    bibcode: list
+
 
 def run_query(query):
     url = 'http://simbad.u-strasbg.fr/simbad/sim-tap/sync'
@@ -52,7 +82,7 @@ def run_query(query):
         raise IndexError(err)
     return response.content.decode()
 
-def parse_table(table, cols=None, values=None):
+def parse_table1(table, cols=None, values=None):
     header = table.splitlines()[0].split('|')
     if cols is None:
         cols = list(map(str.strip, header))
@@ -65,6 +95,29 @@ def parse_table(table, cols=None, values=None):
     values = list(map(str.strip, values))
     values = [value.replace('"', '') for value in values]
     return cols, values
+
+def parse_tablen(table, cols=None, values=None):
+    header = table.splitlines()[0].split('|')
+    cols = list(map(str.strip, header))
+    values = [list(map(str.strip, row.split('|'))) for row in table.splitlines()[2:]]
+    return cols, values
+
+def parse_value(value, err=None, prec=None):
+    try:
+        v = float(value)
+        if err:
+            try:
+                v = ufloat(float(value), float(err))
+            except ValueError:
+                pass
+        if prec:
+            try:
+                v = ufloat(float(value), 10**-int(prec))
+            except ValueError:
+                pass
+    except ValueError:
+        v = np.nan
+    return v
 
 
 effective_temperatures = {
@@ -120,14 +173,37 @@ class simbad:
 
         try:
             table1 = run_query(query=QUERY.format(star=self.star))
-            cols, values = parse_table(table1)
+            cols, values = parse_table1(table1)
 
             table2 = run_query(query=BV_QUERY.format(star=self.star))
-            cols, values = parse_table(table2, cols, values)
+            cols, values = parse_table1(table2, cols, values)
 
             table3 = run_query(query=IDS_QUERY.format(star=self.star))
             line = table3.splitlines()[2]
             self.ids = line.replace('"', '').replace('    ', ' ').replace('   ', ' ').replace('  ', ' ').split('|')
+
+            table4 = run_query(query=FILTERS_QUERY.format(star=self.star))
+            for row in table4.splitlines()[2:]:
+                filter_name, mag, mag_err, bibcode = row.replace('"', '').split('|')
+                filter_name = filter_name.strip()
+                try:
+                    setattr(self, '_' + filter_name, ufloat(float(mag), float(mag_err)))
+                except ValueError:
+                    setattr(self, '_' + filter_name, float(mag))
+
+            # measurements table
+            table5 = run_query(query=MEAS_QUERY.format(star=self.star))
+            _teff, _logg, _feh, _bibcode = [], [], [], []
+            for row in table5.splitlines()[2:]:
+                teff, log_g, log_g_prec, fe_h, fe_h_prec, bibcode = row.replace('"', '').split('|')
+                _bibcode.append(bibcode)
+                _teff.append(parse_value(teff))
+                _logg.append(parse_value(log_g, prec=log_g_prec))
+                _feh.append(parse_value(fe_h, prec=fe_h_prec))
+
+            self.measurements = Measurements(_teff, _logg, _feh, _bibcode)
+
+
         except IndexError:
             raise ValueError(f'simbad query for {star} failed')
 
@@ -176,6 +252,9 @@ class simbad:
         sp_type = self.sp_type
         return f'{self.star} ({V=}, {sp_type=})'
 
+    @property
+    def bmv(self):
+        return self.B - self.V
 
 
 def argsort_by_spectral_type(sptypes):
