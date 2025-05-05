adc509a2a7b058fd46c0c61fd5eb1ab84f134290
João Faria <joao.faria@unige.ch>
2025-05-05 11:18:54 +0200

    get logger from a function, not the module; deal with marimo logger issue

--- arvi/setup_logger.py
+++ arvi/setup_logger.py
@@ -1,20 +1,24 @@
 import sys
-from loguru import logger
 
-try:
-    import marimo as mo
-    if mo.running_in_notebook():
-        raise ImportError
-except (ImportError, ModuleNotFoundError):
-    pass
-else:
-    logger.remove()
+def setup_logger():
+    from loguru import logger
+    try:
+        import marimo as mo
+        if mo.running_in_notebook():
+            raise NotImplementedError
+    except NotImplementedError:
+        pass
+    except (ImportError, ModuleNotFoundError):
+        logger.remove()
+    else:
+        logger.remove()
 
+    logger.configure(extra={"indent": ""})
+    logger.add(
+        sys.stdout,
+        colorize=True,
+        # format="<green>{time:YYYY-MM-DDTHH:mm:ss}</green> <level>{message}</level>",
+        format="{extra[indent]}<level>{message}</level>",
+    )
 
-logger.configure(extra={"indent": ""})
-logger.add(
-    sys.stdout,
-    colorize=True,
-    # format="<green>{time:YYYY-MM-DDTHH:mm:ss}</green> <level>{message}</level>",
-    format="{extra[indent]}<level>{message}</level>",
-)
+    return logger
