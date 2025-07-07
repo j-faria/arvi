792c3a2ac4e98cc630c1582c6070e81c646691d3
João Faria <joao.faria@unige.ch>
2025-07-07 18:52:00 +0200

    allow for GPmodel in run_kima

--- arvi/kima_wrapper.py
+++ arvi/kima_wrapper.py
@@ -1,13 +1,13 @@
 import os
 import numpy as np
 
-from .setup_logger import logger
+from .setup_logger import setup_logger
 
 try:
     import kima
     from kima.pykima.utils import chdir
     from kima import distributions
-    from kima import RVData, RVmodel
+    from kima import RVData, RVmodel, GPmodel
     kima_available = True
 except ImportError:
     kima_available = False
@@ -21,9 +21,12 @@ def try_to_guess_prior(model, prior):
     return None
 
 
-def run_kima(self, run=False, load=False, run_directory=None, priors={}, **kwargs):
+def run_kima(self, run=False, load=False, run_directory=None, model=RVmodel, priors={},
+             **kwargs):
     if not kima_available:
         raise ImportError('kima not available, please install with `pip install kima`')
+    
+    logger = setup_logger()
 
     instruments = [inst for inst in self.instruments if self.NN[inst] > 1]
     time = [getattr(self, inst).mtime for inst in instruments]
@@ -33,12 +36,29 @@ def run_kima(self, run=False, load=False, run_directory=None, priors={}, **kwarg
 
     fix = kwargs.pop('fix', False)
     npmax = kwargs.pop('npmax', 1)
-    model = RVmodel(fix=fix, npmax=npmax, data=data)
+
+    if isinstance(model, str):
+        try:
+            model = {
+                'RVmodel': RVmodel,
+                'GPmodel': GPmodel,
+            }[model]
+        except KeyError:
+            raise ValueError(f'unknown model: {model}')
+    
+    model = model(fix=fix, npmax=npmax, data=data)
 
     model.trend = kwargs.pop('trend', False)
     model.degree = kwargs.pop('degree', 0)
 
-    model.studentt = kwargs.pop('studentt', False)
+    if isinstance(model, RVmodel):
+        model.studentt = kwargs.pop('studentt', False)
+
+    if isinstance(model, GPmodel):
+        if 'kernel' in kwargs:
+            model.kernel = kwargs.pop('kernel')
+
+
     model.enforce_stability = kwargs.pop('enforce_stability', False)
     model.star_mass = kwargs.pop('star_mass', 1.0)
 
