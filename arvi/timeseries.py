1fea4a08d62a638c3507be7d89fce5612337160e
João Faria <joao.faria@unige.ch>
2025-09-01 14:39:49 +0200

    imports

--- arvi/timeseries.py
+++ arvi/timeseries.py
@@ -7,7 +7,6 @@ import warnings
 from copy import deepcopy
 from datetime import datetime, timezone
 
-# import lazy_loader as lazy
 import numpy as np
 
 from .setup_logger import setup_logger
@@ -26,7 +25,8 @@ from .HZ import getHZ_period
 from .instrument_specific import ISSUES
 from .reports import REPORTS
 from .utils import sanitize_path, strtobool, there_is_internet, timer, chdir
-# from .utils import lazy_import
+from .setup_logger import setup_logger
+logger = setup_logger()
 
 # units = lazy_import('astropy.units')
 # units = lazy.load('astropy.units')
