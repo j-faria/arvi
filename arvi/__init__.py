c231bb1e27a5dc5bbdf2829a874305af6ae83608
João Faria <joao.faria@unige.ch>
2025-11-27 11:36:28 +0100

    fix weird getattr in notebook

--- arvi/__init__.py
+++ arvi/__init__.py
@@ -20,6 +20,7 @@ def __getattr__(name: str):
     if name in (
         '_ipython_canary_method_should_not_exist_',
         '_ipython_display_',
+        '__custom_documentations__',
         '_repr_mimebundle_',
         '__wrapped__'
     ):
